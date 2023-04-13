cc43840c7e0e601abf60f7dd7ea329436fa0307f
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-04-13 15:21:27 -0700

    fixing an issue

--- src/app/shared/services/crud.abstract.service.ts
+++ src/app/shared/services/crud.abstract.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { CachingService } from './caching.service';
 import { map } from 'rxjs';
 
+type responseType<T> = { rs: T[] };
 export abstract class CrudService<T> extends CachingService {
   constructor(private http: HttpClient, private baseUrl: string) {
     super();
@@ -9,23 +10,26 @@ export abstract class CrudService<T> extends CachingService {
 
   readById(id: string) {
     return this.http
-      .get<{ rs: T }>(this.baseUrl + `/${id}`)
+      .get<responseType<T>>(this.baseUrl + `/${id}`)
       .pipe(map((res) => res.rs));
   }
 
   readAll() {
-    return this.http.get<{ rs: T[] }>(this.baseUrl).pipe(map((res) => res.rs));
+    return this.http
+      .get<responseType<T>>(this.baseUrl)
+      .pipe(map((res) => res.rs));
   }
 
   create(data: Partial<T>) {
-    return this.http.post<T>(
-      this.baseUrl + `/admin/full`,
-      JSON.stringify(data)
-    );
+    return this.http
+      .post<responseType<T>>(this.baseUrl + `/admin/full`, JSON.stringify(data))
+      .pipe(map((res) => res.rs));
   }
 
   update(data: Partial<T>) {
-    return this.http.put<T>(this.baseUrl + `/admin/full`, JSON.stringify(data));
+    return this.http
+      .put<responseType<T>>(this.baseUrl + `/admin/full`, JSON.stringify(data))
+      .pipe(map((res) => res.rs));
   }
 
   delete(id: string) {
